f965284844e45e38e03416600289e9332f5df603
Julia Evans <julia@jvns.ca>
2025-12-05 14:43:48 -0500

    add events to the site

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,31 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy({"static": "."});
 
+  // Add a filter to get current date
+  eleventyConfig.addFilter("currentDate", () => {
+    return new Date().toISOString();
+  });
+
+  // Add a filter to format event dates
+  eleventyConfig.addFilter("formatEventDate", (dateString) => {
+    const date = new Date(dateString);
+    const options = {
+      weekday: 'short',
+      month: 'short',
+      day: 'numeric',
+      year: 'numeric',
+      hour: 'numeric',
+      minute: '2-digit',
+      timeZoneName: 'short'
+    };
+    return date.toLocaleDateString('en-US', options);
+  });
+
   return {
     dir: {
       input: ".",
       includes: "_includes",
+      data: "_data",
       output: "_site"
     },
     devServer: {
